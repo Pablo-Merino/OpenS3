38f6e363ea6c0d30f8bd6643012ec00b5fb82382
Squeeks <privacymyass@gmail.com>
2012-04-26 19:48:01 +1000

    Syntax cleanup in ::RackFile

--- lib/opens3/rack_file.rb
+++ lib/opens3/rack_file.rb
@@ -1,22 +1,24 @@
 class RackFile
   def initialize(temp_file, filename, type, bucket, path)
     @temp_file = temp_file.path
-    @filename = filename
-    @type = type
-    @bucket = bucket
-    @path = path
+    @filename  = filename
+    @type      = type
+    @bucket    = bucket
+    @path      = path
   end
 
   def save
     begin
-      key = String.new.random_string
-      file_md5 = Digest::MD5.hexdigest(@temp_file)
+      key         = OpenS3::random_string
+      file_md5    = Digest::MD5.hexdigest(@temp_file)
       folder_name = Digest::MD5.hexdigest("#{@file_md5}-#{Time.now}-#{key}")
-      path = "#{@path}/#{@bucket}/#{@filename}"
+      path        = "#{@path}/#{@bucket}/#{@filename}"
+
       FileUtils.mkdir_p(path)
-      meta = Hash.new
-      meta['type']= @type
-      meta['md5'] = file_md5
+
+      meta             = Hash.new
+      meta['type']     = @type
+      meta['md5']      = file_md5
       meta['filename'] = @filename
       meta = meta.to_yaml
       File.open("#{path}/meta", 'w') do |out|
@@ -24,6 +26,7 @@ class RackFile
       end
       FileUtils.mv(@temp_file, "#{path}/content")
     rescue SystemCallError
+      # TODO instead of just failing, we could log with Rack::CommonLogger
       false
     end
   end
