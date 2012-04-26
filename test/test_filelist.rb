4f8f97d22e9c1280c36e95259a60b64cb5a0d5a3
Squeeks <privacymyass@gmail.com>
2012-04-26 19:44:56 +1000

    Adding tests to ::ServerInfo and ::FileList

---
+++ test/test_filelist.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'test/unit'
+require 'rack/test'
+require 'opens3'
+
+class UploadFormTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    OpenS3.app(".","opens3")
+  end
+
+  def test_filelist_notoken
+    get "/list"
+    assert_equal 400, last_response.status
+    assert_equal "application/json", last_response.content_type
+  end
+
+  def test_filelist_withtoken_nobucket
+    get "/list?token=opens3"
+    assert_equal 403, last_response.status
+  end
+g
+end
