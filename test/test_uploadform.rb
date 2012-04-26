1986e879ab520cbbb86e54d2455226f35fb548ea
Squeeks <privacymyass@gmail.com>
2012-04-26 18:16:51 +1000

    Adding basic tests and Rakefile

---
+++ test/test_uploadform.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'test/unit'
+require 'rack/test'
+require 'opens3'
+
+class UploadFormTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    OpenS3.app(".",".")
+  end
+
+  def test_uploadform
+    get "/"
+    assert last_response.ok?
+    assert_equal "text/html", last_response.content_type
+  end
+
+end
